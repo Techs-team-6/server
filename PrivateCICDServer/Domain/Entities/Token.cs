a6937b95712e8847cd80482d9e44f1205c432847
Danilaa56 <danila.aronov@gmail.com>
2022-05-30 01:14:33 +0300

    SERVER-8 feat: adds Token Entity feat: initial TokenService implementation SERVER-10 feat: initial Token API refactor: removes WhetherForecastController SERVER-13 feat: adds NSwag codegen of Server.API.Client

---
+++ PrivateCICDServer/Domain/Entities/Token.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities;
+
+public class Token
+{
+    public Guid Id { get; set; }
+    public string TokenStr { get; set; }
+    public string Description { get; set; }
+    public DateTime CreationTime { get; set; }
+}
--- PrivateCICDServer/Server.API/Startup.cs
+++ PrivateCICDServer/Server.API/Startup.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Server.Core;
+using Server.Core.Services.Implementations;
+using Server.Core.Services.Interfaces;
 
 namespace Server.API;
 
@@ -14,12 +16,14 @@ public class Startup
 
     public void ConfigureServices(IServiceCollection services)
     {
-        services.AddControllers();
         services.AddDbContext<ServerDBContext>(
             options => options.UseSqlite(Configuration.GetConnectionString("Sqlite")!));
+        
+        services.AddScoped<ITokenService, TokenService>();
 
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen();
+        services.AddControllers();
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -35,5 +39,7 @@ public class Startup
         app.UseRouting();
         app.UseAuthorization();
         app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        // app.UseOpenApi();
+        // app.UseSwaggerUi3();
     }
 }
