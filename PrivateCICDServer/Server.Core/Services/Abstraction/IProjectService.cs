6d16a0cce3b3fce5492f439336f5c378a6c04039
eeeedgar <oakmillennial@gmail.com>
2022-05-29 17:59:00 +0300

    feat: add ProjectService interface

---
+++ PrivateCICDServer/Server.Core/Services/Abstraction/IProjectService.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+
+namespace Server.Core.Services.Abstraction;
+
+public interface IProjectService
+{
+    public void RegisterProject(Project project);
+
+    public IReadOnlyList<Project> GetProjects();
+
+    public Project GetProject(Guid id);
+
+    public void UpdateProject(Project project);
+
+    public void DeleteProject(Guid id);
+
+    public IReadOnlyList<Project> FindProjects(string substring);
+}
