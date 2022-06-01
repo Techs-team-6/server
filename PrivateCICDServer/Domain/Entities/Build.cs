f2080d35ce0c154d7333c761d7214022be70265c
Danilaa56 <danila.aronov@gmail.com>
2022-06-01 13:56:31 +0300

    refactor: Build Entity stores Guid StorageId but not BuildUrl

--- PrivateCICDServer/Domain/Entities/Build.cs
+++ PrivateCICDServer/Domain/Entities/Build.cs
@@ -5,6 +5,7 @@ public class Build
     public Guid Id { get; set; }
     public Guid ProjectId { get; set; }
     public Project Project { get; set; }
+    
     public string Name { get; set; }
-    public string BuildUrl { get; set; }
+    public Guid StorageId { get; set; }
 }
