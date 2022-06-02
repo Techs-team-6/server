be1f6ed6779ce52841cc38f7f194da6852585377
vekajp <belka.v.2002@gmail.com>
2022-06-03 02:07:26 +0300

    refactor: clean code

--- PrivateCICDServer/Domain/Entities/InstanceState.cs
+++ PrivateCICDServer/Domain/Entities/InstanceState.cs
@@ -3,6 +3,7 @@ namespace Domain.States;
 public enum InstanceState
 {
     NotPublished,
+    Installing,
     Running,
     Stopped,
 }
--- PrivateCICDServer/Domain/Entities/InstanceStateChange.cs
+++ PrivateCICDServer/Domain/Entities/InstanceStateChange.cs
@@ -4,16 +4,6 @@ namespace Domain.Entities;
 
 public class InstanceStateChange
 {
-    public InstanceStateChange()
-    {
-        
-    }
-    public InstanceStateChange(DateTime changeDate, InstanceState previousState, InstanceState currentState)
-    {
-        ChangeDate = changeDate;
-        PreviousState = previousState;
-        CurrentState = currentState;
-    }
     public Guid Id { get; set; }
     public DateTime ChangeDate { get; set; }
     public InstanceState PreviousState { get; set; }
