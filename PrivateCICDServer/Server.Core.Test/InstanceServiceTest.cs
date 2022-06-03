f3c959dad41903484d6be50d89a9c91f8b5aab41
vekajp <belka.v.2002@gmail.com>
2022-06-03 13:22:13 +0300

    fix tests to fit fixes

--- PrivateCICDServer/Server.Core.Test/InstanceServiceTest.cs
+++ PrivateCICDServer/Server.Core.Test/InstanceServiceTest.cs
@@ -1,4 +1,5 @@
 using System.Runtime.Serialization;
+using Domain.Dto.DedicatedMachineDto;
 using Domain.Entities;
 using Domain.Services;
 using Domain.States;
@@ -17,6 +18,7 @@ public class InsatnceServiceTest
     private List<string> _scripts;
     private Project _testProject;
     private Build _testBuild;
+    private DedicatedMachine _testMachine;
 
     [SetUp]
     public void SetUp()
@@ -36,20 +38,19 @@ public class InsatnceServiceTest
             .ToList();
         
         var projectService =  new ProjectService(context, new ProjectServiceClient("test", new HttpClient()));
+        var tokenSevice = new TokenService(context);
+        var dmService = new DedicatedMachineService(context, tokenSevice);
         _testProject = projectService.CreateProject(_names[0], _scripts[0]);
         _testBuild = projectService.AddBuild(_testProject.Id, _names[0], Guid.NewGuid());
+        _testMachine =
+            dmService.RegisterMachine(new RegisterDto(tokenSevice.Generate("description"), "label", "description"));
     }
 
     [Test]
     public void RegisterInstanceTest()
     {
-        _service.RegisterInstance(_testProject.Id,
-            new InstanceConfig()
-            {
-                Build = _testBuild,
-                StartString = "echo test",
-            },
-            InstanceState.NotPublished);
+        _service.RegisterInstance(_testProject.Id, InstanceState.NotPublished, "start",
+            _testBuild.Id, _testMachine.Id);
         
         Assert.That(_service.ListAllStates(_testProject.Id, 
             _testProject.Instances.FirstOrDefault().Id).Count, Is.EqualTo(1));
@@ -59,12 +60,9 @@ public class InsatnceServiceTest
     public void ChangeInstanceStateTest()
     {
         var instance = _service.RegisterInstance(_testProject.Id,
-            new InstanceConfig()
-            {
-                Build = _testBuild,
-                StartString = "echo test",
-            },
-            InstanceState.NotPublished);
+            InstanceState.NotPublished,
+            "start",
+            _testBuild.Id, _testMachine.Id);
         
         Assert.That(_service.ListAllStates(_testProject.Id, instance.Id).Last(), Is.EqualTo(InstanceState.NotPublished));
         _service.ChangeInstanceState(_testProject.Id, instance.Id, InstanceState.Running);
