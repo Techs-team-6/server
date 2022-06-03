f260e214715af5bcb08959e9e9407ee166947c8b
vekajp <belka.v.2002@gmail.com>
2022-06-03 12:03:25 +0300

    feat: add instance service basic testing

---
+++ PrivateCICDServer/Server.Core.Test/InstanceServiceTest.cs
@@ -0,0 +1,73 @@
+using System.Runtime.Serialization;
+using Domain.Entities;
+using Domain.Services;
+using Domain.States;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using ProjectServiceApiClient;
+using Server.Core.Services;
+
+namespace Server.Core.Test;
+
+public class InsatnceServiceTest
+{
+    private IInstanceService _service;
+    private const int Count = 10;
+    private List<string> _names;
+    private List<string> _scripts;
+    private Project _testProject;
+    private Build _testBuild;
+
+    [SetUp]
+    public void SetUp()
+    {
+        Environment.SetEnvironmentVariable("WITHOUT_PS", "true");
+        var options = new DbContextOptionsBuilder<ServerDbContext>()
+            .UseInMemoryDatabase("Test").Options;
+        var context = new ServerDbContext(options);
+        context.Database.EnsureDeleted();
+        context.Database.EnsureCreated();
+        _service = new InstanceService(context);
+        _names = Enumerable.Range(0, Count)
+            .Select(i => $"name{i}")
+            .ToList();
+        _scripts = Enumerable.Range(0, Count)
+            .Select(i => $"script{i}")
+            .ToList();
+        
+        var projectService =  new ProjectService(context, new ProjectServiceClient("test", new HttpClient()));
+        _testProject = projectService.CreateProject(_names[0], _scripts[0]);
+        _testBuild = projectService.AddBuild(_testProject.Id, _names[0], Guid.NewGuid());
+    }
+
+    [Test]
+    public void RegisterInstanceTest()
+    {
+        _service.RegisterInstance(_testProject.Id,
+            new InstanceConfig()
+            {
+                Build = _testBuild,
+                StartString = "echo test",
+            },
+            InstanceState.NotPublished);
+        
+        Assert.That(_service.ListAllStates(_testProject.Id, 
+            _testProject.Instances.FirstOrDefault().Id).Count, Is.EqualTo(1));
+    }
+    
+    [Test]
+    public void ChangeInstanceStateTest()
+    {
+        var instance = _service.RegisterInstance(_testProject.Id,
+            new InstanceConfig()
+            {
+                Build = _testBuild,
+                StartString = "echo test",
+            },
+            InstanceState.NotPublished);
+        
+        Assert.That(_service.ListAllStates(_testProject.Id, instance.Id).Last(), Is.EqualTo(InstanceState.NotPublished));
+        _service.ChangeInstanceState(_testProject.Id, instance.Id, InstanceState.Running);
+        Assert.That(_service.ListAllStates(_testProject.Id, instance.Id).Last(), Is.EqualTo(InstanceState.Running));
+    }
+}
