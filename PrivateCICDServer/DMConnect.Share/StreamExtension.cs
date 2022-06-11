9447144712b23de0e0042d9ab81e686f71aab2aa
Danilaa56 <danila.aronov@gmail.com>
2022-06-11 18:54:58 +0300

    refactor: use disposable array in stream.ReadString, stream.ReadInt

--- PrivateCICDServer/DMConnect.Share/StreamExtension.cs
+++ PrivateCICDServer/DMConnect.Share/StreamExtension.cs
@@ -27,7 +27,8 @@ public static class StreamExtension
 
     private static int ReadInt(this Stream stream)
     {
-        return (stream.ReadByte() << 24) | (stream.ReadByte() << 16) | (stream.ReadByte() << 8) | (stream.ReadByte());
+        using var bytes = stream.ReadNBytes(4);
+        return (bytes.Value[0] << 24) | (bytes.Value[1] << 16) | (bytes.Value[2] << 8) | bytes.Value[3];
     }
 
     private static void WriteInt(this Stream stream, int value)
@@ -40,25 +41,46 @@ public static class StreamExtension
 
     private static string ReadString(this Stream stream)
     {
-        var size = ReadInt(stream);
-        var bytes = new byte[size];
-
-        var bytesRead = 0;
-        while (bytesRead < size)
-        {
-            var read = stream.Read(bytes, bytesRead, size - bytesRead);
-            if (read == -1)
-                throw new Exception("Stream return EOF");
-            bytesRead += read;
-        }
-
-        return Encoding.UTF8.GetString(bytes, 0, size);
+        var size = stream.ReadInt();
+        using var bytes = stream.ReadNBytes(size);
+        return Encoding.UTF8.GetString(bytes.Value, 0, size);
     }
 
     private static void WriteString(this Stream stream, string value)
     {
         var bytes = Encoding.UTF8.GetBytes(value);
-        WriteInt(stream, bytes.Length);
-        stream.Write(bytes);
+        stream.WriteInt(bytes.Length);
+        stream.Write(bytes, 0, bytes.Length);
+    }
+    
+    /// <summary>
+    /// Reads exact bytes count
+    /// </summary>
+    /// <param name="stream">Source of data</param>
+    /// <param name="length">Exact bytes count to read</param>
+    /// <returns>Disposable byte array with data</returns>
+    /// <exception cref="IOException"></exception>
+    private static DisposableByteArray ReadNBytes(this Stream stream, int length)
+    {
+        var bytes = new DisposableByteArray(length);
+
+        try
+        {
+            var bytesRead = 0;
+            while (bytesRead < length)
+            {
+                var read = stream.Read(bytes.Value, bytesRead, length - bytesRead);
+                if (read == -1)
+                    throw new IOException($"Stream returned EOF. Expected {length} bytes, read {bytesRead}");
+                bytesRead += read;
+            }
+        }
+        catch (Exception)
+        {
+            bytes.Dispose();
+            throw;
+        }
+
+        return bytes;
     }
 }
