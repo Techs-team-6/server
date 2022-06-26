16049bd9f0c63ff4642ad4fd7964c55a261766e2
vekajp <belka.v.2002@gmail.com>
2022-06-27 02:01:38 +0300

    fix error parser, parse error message and show

--- PrivateCICDServer/Server.BlazorUI/Tools/ErrorParser.cs
+++ PrivateCICDServer/Server.BlazorUI/Tools/ErrorParser.cs
@@ -1,23 +1,16 @@
-﻿namespace Server.BlazorUI.Tools;
+﻿using System.Text.Json;
+using Domain.Tools;
+
+namespace Server.BlazorUI.Tools;
 
 public class ErrorParser
 {
     public static string GetErrorMessage(string description)
     {
-        var responseIndex = description.IndexOf("Response: ", StringComparison.Ordinal);
+        var responseIndex = description.Split("Response: ");
 
-        responseIndex += 12;
-        var lastResponseSymbol = description.Length - 2;
-
-        return description.Substring(responseIndex, lastResponseSymbol - responseIndex);
-    }
-
-    public static string GetErrorCode(string description)
-    {
-        var statusIndex = description.IndexOf("Status: ", StringComparison.Ordinal);
-
-        statusIndex += 8;
-
-        return description.Substring(statusIndex, 3);
+        if (responseIndex.Length <= 1) return description;
+        var details = JsonSerializer.Deserialize<ErrorDetails>(responseIndex[1][..^1]);
+        return details.Message;
     }
 }
