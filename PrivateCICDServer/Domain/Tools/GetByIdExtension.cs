dff9bcd19307b18dd682cd90bd75b20b3fd544d2
Danilaa56 <danila.aronov@gmail.com>
2022-06-26 19:05:17 +0300

    fix: typename in GetById exception message fix: OnMachineAgentLeave Id check

--- PrivateCICDServer/Domain/Tools/GetByIdExtension.cs
+++ PrivateCICDServer/Domain/Tools/GetByIdExtension.cs
@@ -5,6 +5,6 @@ public static class GetByIdExtension
     public static T GetById<T>(this IEnumerable<T> collection, Guid id) where T : IHasId
     {
         return collection.FirstOrDefault(i => i.Id == id) ??
-               throw new EntityNotFoundException(nameof(T), id);
+               throw new EntityNotFoundException(typeof(T).Name, id);
     }
 }
